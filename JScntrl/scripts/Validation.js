c3dc9c64e55dee62bb85ed534b644f2c922dc502
Stepan Abramov <warhammer.asv@yandex.ru>
2016-04-26 22:51:25 +0300

    Доделана валидация на интовые поля

--- JScntrl/scripts/Validation.js
+++ JScntrl/scripts/Validation.js
@@ -1,20 +1,31 @@
 ﻿function IntegerValidation(txb) {
+
     if ((txb.value.startsWith("-"))) {
-        txb.value = txb.value.replace(/[^0-9]/ig, "");
-        txb.value = "-" + txb.value;
-        txb.style.border = "2px solid green"
+        if ((!isNaN(txb.value.charAt(1))) && (txb.value.charAt(1)>=1)) {
+            txb.value = txb.value.replace(/[^0-9]/ig, "");
+            txb.value = "-" + txb.value;
+            txb.style.border = "2px solid green"
+        }
+        else {
+            txb.value = txb.value.replace(/[^1-9]/ig, "");
+            txb.value = "-" + txb.value;
+            txb.style.border = "2px solid green"
+        }
     }
     else if (txb.value.startsWith("0")) {
         txb.value = "0";
     }
     else {
-        txb.value = txb.value.replace(/[^0-9]/ig, "");
+        txb.value = txb.value.replace(/[^\d]/ig, "");
         txb.style.border = "2px solid green"
     }
     if (txb.value > 255) {
-        txb.value = "255";
+        alert("Значение должно быть меньше 256")
+        txb.style.border = "2px solid red";
     }
-       if (txb.value < -255) {
-        txb.value = "-255";
+    if (txb.value < -255) {
+        alert("Значение должно быть больше -256")
+        txb.style.border = "2px solid red";
     }
+
 }
